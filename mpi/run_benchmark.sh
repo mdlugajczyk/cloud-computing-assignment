76b96d424b358fb5fff2628f34ca41372e576ee1
Marcin Długajczyk <dlugajczykmarcin@gmail.com>
2014-03-05 16:58:40 +0000

    Improves script for running mpi program.

--- mpi/run_benchmark.sh
+++ mpi/run_benchmark.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-INPUT1=m1.txt
-INPUT2=m2.txt
-OUTPUT=output.txt
-
-if [ $# -ne 2 ]
+function generate_input {
+    if [ ! -f $1 ]
 then
-    echo "Usage: $0 rows cols"
+    ./generate $2 $3 > $1
+fi  
+}
+
+if [ $# -ne 3 ]
+then
+    echo "Usage: $0 nodes rows cols";
+    exit 1;
 fi
 
-make 1> /dev/null;
-./generate $1 $2 > $INPUT1
-./generate $1 $2 > $INPUT2
-./multiply $INPUT1 $INPUT2 $OUTPUT
+INPUT1="input1_$2_$3.txt"
+INPUT2="input2_$2_$3.txt"
+OUTPUT="output_$2_$2.txt"
+
+echo "Compiling program..."
+make all 1> /dev/null;
+
+echo "Generating input files..."
+generate_input $INPUT1 $2 $3
+generate_input $INPUT2 $2 $3
+
+echo "Uploading executable to nodes..."
+while read host; do
+    scp multiply $host: 1> /dev/null
+done < ../mpi.host
+
+root_node=$(cat ../mpi.host |head -n 1)
+scp $INPUT1 $root_node:
+scp $INPUT2 $root_node:
+
+mpirun -n $1 -machinefile ../mpi.host ./multiply $INPUT1 $INPUT2 $OUTPUT
